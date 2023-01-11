6cb9e7408531f26271ce89463ac537d1f30bf169
BigCat <bigcat@BigCatdeMBP.lan>
2023-01-11 21:48:31 +0800

    优化配置

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,51 @@ export default defineConfig(({ mode }) => {
         plugins: [ createPostcssPxtoremPlugin(), createAutoPrefixerPlugin() ]
       }
     },
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: "js/[name]-[hash].js",
+          entryFileNames: "js/[name]-[hash].js",
+          assetFileNames({ name }) {
+            if(/\.(jpe?g|png|gif|webp|svg)$/i.test(name)) {
+              return "images/[name]-[hash].[ext]";
+            }
+            if(/\.svg$/i.test(name)) {
+              return "svg/[name]-[hash].[ext]";
+            }
+            if(/\.(woff|woff2|eot|ttf|otf)$/i.test(name)) {
+              return "fonts/[name]-[hash].[ext]";
+            }
+            if(/\.css$/i.test(name)) {
+              return "css/[name]-[hash].[ext]";
+            }
+            return "[name]-[hash].[ext]";
+          },
+          manualChunks: (filePath) => {
+            filePath = filePath.split("?")[0];
+            if(/[\\/]vant[\\/]/i.test(filePath)) {
+              return "vant";
+            }
+            if(/[\\/]@?vue[\\/]/i.test(filePath)) {
+              return "vue";
+            }
+            if(/[\\/]vue-router[\\/]/i.test(filePath)) {
+              return "vue-router";
+            }
+            /* index文件取上级目录 */
+            const IndexVueFileRegExp = /[\\/](?<name>[^\\/]+)[\\/]index\.vue$/i;
+            const result = filePath.match(IndexVueFileRegExp);
+            if(result) {
+              return result.groups.name
+            }
+            /* 其他依赖处理 */
+            if(/[\\/]node_modules[\\/]/i.test(filePath)) {
+              return "vendor";
+            }
+          }
+        }
+      }
+    },
     plugins: [
       vue(),
       VueSetupExtend(),
