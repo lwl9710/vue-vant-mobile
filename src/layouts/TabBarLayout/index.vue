7b04c6b3df3aaa3500545c70510ab5efd692395f
LWL <766649508@qq.com>
2023-01-11 18:12:45 +0800

    增加初始化样式

---
+++ src/layouts/TabBarLayout/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view></router-view>
+  <Tabbar></Tabbar>
+</template>
+<script lang="ts" setup>
+import Tabbar from "./components/Tabbar/index.vue";
+</script>
+<style lang="scss" scoped>
+
+</style>
