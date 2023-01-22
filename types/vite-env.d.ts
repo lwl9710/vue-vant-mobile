ab0bf55fb7510a55618670e709877475c279429c
BigCat <bigcat@BigCatdeMBP.lan>
2023-01-22 21:13:09 +0800

    调整声明

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-declare module "nprogress" {
-  interface NProgress {
-    start(): void;
-    done(): void;
-  }
-  const instance: NProgress;
-  export default instance;
-}
